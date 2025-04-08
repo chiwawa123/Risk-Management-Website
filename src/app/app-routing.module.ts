ad1fd8ee92a0b24ba3e73e87d1238ab8a4eb1069
chiwawa123 <tauraishep@gmail.com>
2025-04-08 15:31:41 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DownloadsComponent } from './components/downloads/downloads.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { StaffComponent } from './components/staff/staff.component';
+import { ServicesComponent } from './components/services/services.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent}
+  {path:'',component:HomeComponent},
+  {path:'staff',component:StaffComponent},
+  {path:'about',component:AboutComponent},
+  {path:'downloads',component:DownloadsComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'services',component:ServicesComponent}
 ];
 
 @NgModule({
